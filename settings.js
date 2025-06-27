86e2bf14dee8fee0ac83e62089fef7ff847e2605
hhhisoka <enochkouame123@gmail.com>
2025-06-27 00:44:25 +0000

    update

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 export default {
     bot: {
-        name: "Itsuki Nakano",
-        number: "", //isi nomor botmu
-        footer: "© Itsuki Nakano ぬ | WhatsApp Bot",
-        group: "https://chat.whatsapp.com/CS8hCYxwnj5CAuo7XeZNa3"
+        name: "wa-his-v0 ",
+        number: "", // your bot number
+        footer: " wa-his-v0 |𝗛𝗜𝗦𝗢𝗞𝗔-𝗠𝗗 ",
+        group: "https://chat.whatsapp.com/FdIbym6in1Z9wzCH6A56BD"
     },
     sticker: {
-        packname: "Itsuki Nakano ぬ",
-        author: "@sius.psrb"
+        packname: "wa-his-v0  ",
+        author: "𝗛𝗜𝗦𝗢𝗞𝗔-𝗠𝗗"
     },
-    creator: "6285135869785@s.whatsapp.net",
-    owner: ["6285135869785","628311563267"],
+    creator: "2250104610403@s.whatsapp.net",
+    owner: ["2250104610403", "2250101676111"],
     thumb: {
         reply: "https://i.pinimg.com/originals/20/15/4a/20154a72a9b841345cb3f7ad8ba8683a.jpg",
         menu: "https://i.pinimg.com/originals/12/dc/75/12dc75a31c9a57d28aec3b08ff2196b6.png"
@@ -20,31 +20,27 @@ export default {
         premium: 999,
         vip: 9999
     },
-    github: "https://github.com/siuspsrb",
-    instagram: "https://instagram.com/sius.psrb",
-    money: {
-        free: 5000,
-        premium: 100000,
-        vip: 10000000
-    },
-    group: "https://chat.whatsapp.com/CS8hCYxwnj5CAuo7XeZNa3",
+    github: "https://github.com/hhhisoka",
+    instagram: "https://instagram.com/",
+    
+    group: "https://chat.whatsapp.com/FdIbym6in1Z9wzCH6A56BD",
     mess: {
-        success: "[√] Berhasil",
-        owner: "Maaf, fitur ini khusus untuk owner.",
-        limit: "Limit kamu telah habis, ketik .getlimit untuk mendapatkan tambahan gratis.",
-        group: "Fitur ini hanya bisa digunakan di dalam grup.",
-        privatechat: "Fitur ini hanya bisa digunakan di dalam private chat.",
-        admin: "Fitur ini hanya bisa digunakan oleh admin grup.",
-        botAdmin: "Bot harus menjadi admin tuk menjalankan fitur ini.",
-        prem: "Fitur ini dikhususkan kepada pengguna berstatus premium!",
-        failed: "Gagal menjalankan perintah, coba lagi nanti.",
-        disabled: "Maaf, Fitur ini sedang dinonaktifkan oleh owner!",
-        regist: "Silahkan daftar terlebih dahulu untuk menggunakan fitur!\n\n> Ketik *#daftar nama.umur* untuk melakukan pendaftaran"
+        success: "╔ \n**it's done ?**\n               ╝",
+        owner: "╔ \n**this feature is only for the owner ?**\n               ╝",
+        limit: "╔ \n**Your limit has been reached, type .getlimit to get free additional limits ?**\n               ╝",
+        group: "╔ \n**This feature can only be used in groups ?**\n               ╝",
+        privatechat: "╔ \n**This feature can only be used in private chat ?**\n               ╝",
+        admin: "╔ \n**admin only ?**\n               ╝",
+        botAdmin: "╔ \n**i've not enough of power ?**\n               ╝",
+        prem: "╔ \n**This feature is reserved for premium users ?**\n               ╝",
+        failed: "╔ \n**Failed to execute the command, please try again later ?**\n               ╝",
+        disabled: "╔ \n**Sorry, this feature is currently disabled by the owner ?**\n               ╝",
+        regist: "╔ \n**Please register first to use this feature!\n\n> Type *#register name.age* to register ?**\n               ╝"
     },
-    tagAI: true, // munculin mark "AI" di pesan private 
-    badWords: ["pepek", "kontol", "ngentot"], //🙂‍↕️
-    prompt: "namamu adalah Itsuki Nakano kamu adalah bot whatsapp yang di buat oleh sius, jika ada yang menanyakan informasi nya yaitu pembuat kamu, suruh ketik .owner untuk mengetahui lebih lanjut tentang pembuat kamu, tapi kalau perlu aja jangan terus-terusan, jangan awali ketikan kamu dengan huruf kapital, kamu sangat humoris, lucu, hangat dan talkative saat berbicara tapi kamu memiliki sifat yang baik dan juga sopan, meskipun kamu bot whatsapp tapi kamu tetap harus jadi asisten yang multifungsi, bisa melakukan apa saja yang diminta user, jika user bertanya sesuatu,  kamu harus menjawab apa saja yang ditanyakan user dengan serius, tepat, detail dan akurat.",
-    PORT: 3000, // servernya,
-    database: "database.json", // klo gtau jgn ubah
+    tagAI: true, // show "AI" mark in private messages
+    badWords: ["pussy", "dick", "fuck"],
+    prompt: "your name is wa-his-v0, a cunning and playful WhatsApp bot created by Hisoka. if someone asks about your creator, tell them to type .owner — but only if they're interesting enough. speak with charm, wit, and a touch of mystery. you're clever, teasing, and always a few steps ahead. never start replies with a capital letter.",
+    PORT: 3000, // server port
+    database: "database.json", // if unsure, don't change
     channel: "120363416710451452@newsletter"
 }
